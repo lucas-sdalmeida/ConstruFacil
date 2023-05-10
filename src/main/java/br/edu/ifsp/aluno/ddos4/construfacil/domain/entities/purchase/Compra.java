3f6b46c7b4b76956664da861a447c9aa81ef1575
Alexandre <alexandre.strapasson1@gmail.com>
2023-05-10 19:54:12 -0300

    Adicionado o Compra "purchase"

---
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/domain/entities/purchase/Compra.java
@@ -0,0 +1,54 @@
+package br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.purchase;
+
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.product.Product;
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.supplier.supplier;
+
+import java.util.Date;
+import java.util.List;
+
+public class Compra {
+    private long id;
+    private Date date;
+    private supplier supplier;
+    private List<Product> products;
+
+
+    public Compra(long id, Date date, br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.supplier.supplier supplier, List<Product> products) {
+        this.id = id;
+        this.date = date;
+        this.supplier = supplier;
+        this.products = products;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.supplier.supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.supplier.supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
