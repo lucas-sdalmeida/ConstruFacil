a1cebe7d3367864b08a6d8471a9d72dd16c9e1d3
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-05-16 20:29:49 -0300

    feat: implemented find supplier use case

---
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/domain/usecases/supplier/FindSupplierUseCase.java
@@ -0,0 +1,35 @@
+package br.edu.ifsp.aluno.ddos4.construfacil.domain.usecases.supplier;
+
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.supplier.Supplier;
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.persistence.dao.SupplierDAO;
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.usecases.util.Validator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public class FindSupplierUseCase {
+    private final SupplierDAO supplierDAO;
+
+    public FindSupplierUseCase(SupplierDAO supplierDAO) {
+        this.supplierDAO = supplierDAO;
+    }
+
+    public Optional<Supplier> findOneById(Long id) {
+        Objects.requireNonNull(id);
+
+        return supplierDAO.findOneByKey(id);
+    }
+
+    public Optional<Supplier> findOneByCNPJ(String cnpj) {
+        if (Validator.isNullOrEmpty(cnpj))
+            throw new IllegalArgumentException("CNPJ is required for this operation!");
+
+        return supplierDAO.findOneByCNPJ(cnpj);
+    }
+
+    public Map<Long, Supplier> findAll() {
+        return supplierDAO.findAll();
+    }
+}
