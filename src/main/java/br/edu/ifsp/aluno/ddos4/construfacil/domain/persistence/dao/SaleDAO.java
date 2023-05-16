372402ee357c37ba2fa355879b464890290e6de5
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-05-16 15:45:41 -0300

    feat: defined SaleDAO and SaleRefundDAO interfaces

---
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/domain/persistence/dao/SaleDAO.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp.aluno.ddos4.construfacil.domain.persistence.dao;
+
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.customer.Customer;
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.sale.Sale;
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.persistence.util.DAO;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+public interface SaleDAO extends DAO<Long, Sale> {
+    Map<Long, Sale> findSomeByDate(LocalDate date);
+
+    Map<Long, Sale> findSomeByCustomer(Customer customer);
+
+    Map<Long, Sale> findSomeByCustomerAndDate(Customer customer, LocalDate date);
+}
---
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/domain/persistence/dao/SaleRefundDAO.java
@@ -0,0 +1,11 @@
+package br.edu.ifsp.aluno.ddos4.construfacil.domain.persistence.dao;
+
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.sale.Sale;
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.sale.SaleRefund;
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.persistence.util.DAO;
+
+import java.util.Optional;
+
+public interface SaleRefundDAO extends DAO<Long, SaleRefund> {
+    Optional<SaleRefund> findOneBySale(Sale Sale);
+}
