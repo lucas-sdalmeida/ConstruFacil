92e54a21bc2fe782a5b269007f4f2257e8ae18fb
AlexandreStrpasson <113216865+AlexandreStrpasson@users.noreply.github.com>
2023-05-17 19:59:08 -0300

    DAOSupplier had benn rename to SupplierSQliteDAO

--- src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/application/repositories/sqlite/dao/SupplierSQliteDAO.java
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/application/repositories/sqlite/dao/SupplierSQliteDAO.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import java.util.Map;
 import java.util.Optional;
 
-public class DAOSupplier implements SupplierDAO {
+public class SupplierSQliteDAO implements SupplierDAO {
 
     @Override
     public void save(Supplier supplier) {
