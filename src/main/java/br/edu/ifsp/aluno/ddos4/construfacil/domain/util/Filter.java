5153fd57cf5a4ac70c1b4424889d18df2f305523
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-05-21 17:30:45 -0300

    feat: added the Filter class to use when filtering values on reports

---
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/domain/util/Filter.java
@@ -0,0 +1,53 @@
+package br.edu.ifsp.aluno.ddos4.construfacil.domain.util;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Filter<A> {
+    private final Set<A> allowedValuesSet = new HashSet<>();
+    private final Set<A> forbiddenValuesSet = new HashSet<>();
+
+    public final boolean applyTo(A value) {
+        Objects.requireNonNull(value);
+
+        if (allowedValuesSet.isEmpty())
+            return !forbiddenValuesSet.contains(value);
+
+        return allowedValuesSet.contains(value);
+    }
+
+    public final void addToAllowedValuesSet(A value) {
+        Objects.requireNonNull(value);
+
+        if (forbiddenValuesSet.contains(value))
+            throw new ValueAlreadyFilteredException(
+                "Cannot add such value because it is on the forbidden values set!"
+            );
+
+        allowedValuesSet.add(value);
+    }
+
+    public final void removeFromAllowedValuesSet(A value) {
+        Objects.requireNonNull(value);
+
+        allowedValuesSet.remove(value);
+    }
+
+    public final void addToForbiddenValuesSet(A value) {
+        Objects.requireNonNull(value);
+
+        if (allowedValuesSet.contains(value))
+            throw new ValueAlreadyFilteredException(
+                    "Cannot add this value because it is on the allowed values set!"
+            );
+
+        forbiddenValuesSet.add(value);
+    }
+
+    public final void removeFromForbiddenValuesSet(A value) {
+        Objects.requireNonNull(value);
+
+        forbiddenValuesSet.remove(value);
+    }
+}
---
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/domain/util/ValueAlreadyFilteredException.java
@@ -0,0 +1,7 @@
+package br.edu.ifsp.aluno.ddos4.construfacil.domain.util;
+
+public class ValueAlreadyFilteredException extends RuntimeException {
+    public ValueAlreadyFilteredException(String message) {
+        super(message);
+    }
+}
