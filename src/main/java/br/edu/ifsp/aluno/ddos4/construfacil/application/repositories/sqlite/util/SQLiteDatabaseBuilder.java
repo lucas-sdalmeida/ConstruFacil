c06a8df0f1006aad0f43633a737bfddb70ea9ee4
AlexandreStrpasson <113216865+AlexandreStrpasson@users.noreply.github.com>
2023-06-10 00:01:09 -0300

    update to SQlite commands

--- src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/application/repositories/sqlite/util/SQLiteDatabaseBuilder.java
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/application/repositories/sqlite/util/SQLiteDatabaseBuilder.java
@@ -52,7 +52,7 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         builder.append("CPF VARCHAR(15) NOT NULL, \n");
         builder.append("address VARCHAR(100), \n");
         builder.append("phone_Number VARCHAR(20), \n");
-        builder.append("status VARCHAR(10) DEFUALT 'ACTIVE', \n");
+        builder.append("status VARCHAR(10) DEFAULT 'ACTIVE', \n");
         builder.append("CONSTRAINT customer_pk PRIMARY KEY (id_customer),");
         builder.append("CONSTRAINT cpf_un UNIQUE (CPF)");
         builder.append(");\n");
@@ -71,9 +71,9 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         builder.append("average_Purchase_Price NUMBER, \n");
         builder.append("status VARCHAR(10) NOT NULL DEFAULT 'ACTIVE', \n");
         builder.append("CONSTRAINT product_pk PRIMARY KEY (id_product), \n");
-        builder.append("CONSTRAINT name_un  UNIQUE (name), \n");
-        builder.append("CONSTRAINT quantity_ck CHECK (quantity > 0), \n");
-        builder.append("CONSTRAINT average_purchase_price_ck UNIQUE (average_purchase_price > 0) \n");
+        builder.append("CONSTRAINT name_un UNIQUE (name), \n");
+        builder.append("CONSTRAINT quantity_ck CHECK (quantity > 0) \n");
+        //builder.append("CONSTRAINT average_purchase_price_ck UNIQUE (average_purchase_price > 0) \n");
         builder.append("); \n");
 
         return builder.toString();
@@ -103,7 +103,7 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         builder.append("id_supplier NUMBER NOT NULL, \n");
         builder.append("id_product NUMBER NOT NULL, \n");
         builder.append("purchase_date TIMESTAMP NOT NULL,\n");
-        builder.append("CONSTRAINT purchase_pk PRIMARY KEY purchase(id_purchase),");
+        builder.append("CONSTRAINT purchase_pk PRIMARY KEY (id_purchase),");
         builder.append("CONSTRAINT purchase_supplier_supplies_fk FOREIGN KEY (id_supplier, id_product, purchase_date)");
         builder.append("REFERENCES SUPPLIER_SUPPLIES(id_supplier, id_product, purchase_date)");
         builder.append("); \n");
@@ -126,6 +126,24 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         return builder.toString();
     }
 
+    private String createCustomerBuysTableSql() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("CREATE TABLE customer_Buys ( \n");
+        builder.append("id_product NUMBER, \n");
+        builder.append("id_customer NUMBER,\n");
+        builder.append("sale_date TIMESTAMP,\n");
+        builder.append("price NUMBER NOT NULL,\n");
+        builder.append("quantity NUMBER NOT NULL,\n");
+        builder.append("CONSTRAINT customer_buys_pk PRIMARY KEY (id_product, id_customer, sale_date),\n");
+        builder.append("CONSTRAINT price_customer_ck CHECK (price > 0),\n");
+        builder.append("CONSTRAINT quantity_buyies_ck CHECK (quantity > 0)\n");
+        builder.append("); \n");
+
+        System.out.println(builder.toString());
+        return builder.toString();
+    }
+
     private String createSaleTableSql(){
         StringBuilder builder = new StringBuilder();
 
@@ -134,9 +152,9 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         builder.append("id_customer NUMBER NOT NULL, \n");
         builder.append("id_product NUMBER NOT NULL, \n");
         builder.append("sale_date TIMESTAMP NOT NULL,");
-        builder.append("CONSTRAINT sale_pk PRIMARY KEY sale(id_sale),\n");
-        builder.append("CONSTRAINT sale_customer_buys_fk FOREIGN KEY (id_pruduct, id_customer, sale_date)");
-        builder.append("REFERENCES CUSTOMER_BUYS(id_pruduct, id_customer, sale_date)");
+        builder.append("CONSTRAINT sale_pk PRIMARY KEY (id_sale),\n");
+        builder.append("CONSTRAINT sale_customer_buys_fk FOREIGN KEY (id_product, id_customer, sale_date)");
+        builder.append("REFERENCES CUSTOMER_BUYS(id_product, id_customer, sale_date)");
         builder.append("); \n");
 
         System.out.println(builder.toString());
@@ -151,24 +169,6 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         builder.append("id_sale        NUMBER, \n");
         builder.append("CONSTRAINT sale_refund_pk PRIMARY KEY (id_sale_refund),\n");
         builder.append("CONSTRAINT id_sale_refund_fk FOREIGN KEY (id_sale) REFERENCES sale(id_sale)");
-        builder.append("); \n");
-
-        System.out.println(builder.toString());
-        return builder.toString();
-    }
-
-    private String createCustomerBuysTableSql() {
-        StringBuilder builder = new StringBuilder();
-
-        builder.append("CREATE TABLE customer_Buys ( \n");
-        builder.append("id_product NUMBER, \n");
-        builder.append("id_customer NUMBER,\n");
-        builder.append("sale_date TIMESTAMP,\n");
-        builder.append("price NUMBER NOT NULL,\n");
-        builder.append("quantity NUMBER NOT NULL,\n");
-        builder.append("CONSTRAINT customer_buys_pk PRIMARY KEY (id_product, id_customer, sale_date),\n");
-        builder.append("CONSTRAINT price_customer_ck CHECK (price > 0),\n");
-        builder.append("CONSTRAINT quantity_buyies_ck CHECK (quantity > 0)\n");
         builder.append("); \n");
 
         System.out.println(builder.toString());
