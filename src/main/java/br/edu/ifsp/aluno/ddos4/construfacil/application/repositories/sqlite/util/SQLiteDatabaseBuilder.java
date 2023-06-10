b1ec91f923ccd58c1c5df78990ab0294af8700e9
AlexandreStrpasson <113216865+AlexandreStrpasson@users.noreply.github.com>
2023-06-09 22:59:15 -0300

    adjusted the SQL commands

--- src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/application/repositories/sqlite/util/SQLiteDatabaseBuilder.java
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/application/repositories/sqlite/util/SQLiteDatabaseBuilder.java
@@ -42,113 +42,154 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         }
     }
 
-    // todo: substituir o sql da tabela CUSTOMER pelo definitivo
+    //todo: conferir se o default está sendo usado corretamente
     private String createCustomerTableSql(){
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE Customer (\n");
-        builder.append("id INTEGER NOT NULL PRIMARY KEY, \n");
-        builder.append("name TEXT NOT NULL, \n");
-        builder.append("cpf TEXT NOT NULL, \n");
-        builder.append("address TEXT, \n");
-        builder.append("phoneNumber TEXT, \n");
-        builder.append("active INTEGER NOT NULL \n");
-        builder.append("); \n");
+        builder.append("id_customer NUMBER NOT NULL, \n");
+        builder.append("name VARCHAR(100) NOT NULL, \n");
+        builder.append("CPF VARCHAR(15) NOT NULL, \n");
+        builder.append("address VARCHAR(100), \n");
+        builder.append("phone_Number VARCHAR(20), \n");
+        builder.append("status VARCHAR(10) DEFUALT 'ACTIVE', \n");
+        builder.append("CONSTRAINT customer_pk PRIMARY KEY (id_customer),");
+        builder.append("CONSTRAINT cpf_un UNIQUE (CPF)");
+        builder.append(");\n");
 
         return builder.toString();
     }
 
-    // todo: substituir o sql da tabela PRODUCT pela definitiva
+    //todo: conferir o o default está correto
     private String createProductTableSql(){
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE Product (\n");
-        builder.append("id INTEGER NOT NULL PRIMARY KEY, \n");
-        builder.append("name TEXT NOT NULL, \n");
-        builder.append("quantity INTEGER NOT NULL, \n");
-        builder.append("defaultPurchasePrice REAL NOT NULL, \n");
-        builder.append("defaultSalePrice REAL NOT NULL, \n");
-        builder.append("active INTEGER NOT NULL \n");
+        builder.append("id_product NUMBER NOT NULL, \n");
+        builder.append("name VARCHAR(100) NOT NULL, \n");
+        builder.append("quantity NUMBER NOT NULL, \n");
+        builder.append("average_Purchase_Price NUMBER, \n");
+        builder.append("status VARCHAR(10) NOT NULL DEFAULT 'ACTIVE', \n");
+        builder.append("CONSTRAINT product_pk PRIMARY KEY (id_product), \n");
+        builder.append("CONSTRAINT name_un  UNIQUE (name), \n");
+        builder.append("CONSTRAINT quantity_ck CHECK (quantity > 0), \n");
+        builder.append("CONSTRAINT average_purchase_price_ck UNIQUE (average_purchase_price > 0) \n");
         builder.append("); \n");
 
         return builder.toString();
     }
 
-    // todo: substituir a implementação da tabela Supplier
     private String createSupplierTableSql(){
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE Supplier (\n");
-        builder.append("id INTEGER NOT NULL PRIMARY KEY, \n");
-        builder.append("corporateName TEXT NOT NULL, \n");
-        builder.append("cnpj TEXT NOT NULL, \n");
-        builder.append("phoneNumber TEXT, \n");
+        builder.append("id_supplier NUMBER, \n");
+        builder.append("CNPJ VARCHAR(15) NOT NULL, \n");
+        builder.append("corporate_Name VARCHAR(100) NOT NULL, \n");
+        builder.append("Phone_Number VARCHAR(20), \n");
+        builder.append("CONSTRAINT supplier_pk PRIMARY KEY (id_supplier),");
+        builder.append("CONSTRAINT CNPJ_UN UNIQUE (CNPJ),");
+        builder.append("CONSTRAINT corporate_name_un UNIQUE (corporate_name)");
         builder.append("); \n");
 
         return builder.toString();
     }
 
-    // todo: substituir a implementação da tabela Purchase
     private String createPurchaseTableSql(){
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE Purchase (\n");
-        builder.append("id INTEGER NOT NULL PRIMARY KEY, \n");
-        builder.append("date TEXT NOT NULL, \n");
-        builder.append("supplier INTEGER NOT NULL, \n");
-        builder.append("FOREIGN KEY(supplier) REFERENCES Supplier(id)\n");
+        builder.append("id_purchase NUMBER, \n");
+        builder.append("id_supplier NUMBER NOT NULL, \n");
+        builder.append("id_product NUMBER NOT NULL, \n");
+        builder.append("purchase_date TIMESTAMP NOT NULL,\n");
+        builder.append("CONSTRAINT purchase_pk PRIMARY KEY purchase(id_purchase),");
+        builder.append("CONSTRAINT purchase_supplier_supplies_fk FOREIGN KEY (id_supplier, id_product, purchase_date)");
+        builder.append("REFERENCES SUPPLIER_SUPPLIES(id_supplier, id_product, purchase_date)");
         builder.append("); \n");
 
         System.out.println(builder.toString());
         return builder.toString();
     }
 
-    // todo: substituir a implementação da tabela Purchase_Refund
     private String createPurchaseRefundTableSql(){
         StringBuilder builder = new StringBuilder();
 
-        builder.append("CREATE TABLE PurchaseRefund (\n");
-        builder.append("id INTEGER NOT NULL PRIMARY KEY, \n");
-        builder.append("purchase INTEGER NOT NULL, \n");
-        builder.append("FOREIGN KEY(purchase) REFERENCES Purchase(id)\n");
+        builder.append("CREATE TABLE Purchase_Refund (\n");
+        builder.append("id_purchase_refund NUMBER, \n");
+        builder.append("id_purchase NUMBER, \n");
+        builder.append("CONSTRAINT purchase_refund_pk PRIMARY KEY (id_purchase_refund),\n");
+        builder.append("CONSTRAINT id_purchase_refund_fk FOREIGN KEY (id_purchase) REFERENCES purchase(id_purchase)");
         builder.append("); \n");
 
         System.out.println(builder.toString());
         return builder.toString();
     }
 
-    // todo: substituir a implementação da tabela Sale
     private String createSaleTableSql(){
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE Sale (\n");
-        builder.append("id INTEGER NOT NULL PRIMARY KEY, \n");
-        builder.append("date TEXT NOT NULL, \n");
-        builder.append("customer INTEGER NOT NULL, \n");
-        builder.append("FOREIGN KEY(customer) REFERENCES Customer(id)\n");
+        builder.append("id_sale NUMBER, \n");
+        builder.append("id_customer NUMBER NOT NULL, \n");
+        builder.append("id_product NUMBER NOT NULL, \n");
+        builder.append("sale_date TIMESTAMP NOT NULL,");
+        builder.append("CONSTRAINT sale_pk PRIMARY KEY sale(id_sale),\n");
+        builder.append("CONSTRAINT sale_customer_buys_fk FOREIGN KEY (id_pruduct, id_customer, sale_date)");
+        builder.append("REFERENCES CUSTOMER_BUYS(id_pruduct, id_customer, sale_date)");
         builder.append("); \n");
 
         System.out.println(builder.toString());
         return builder.toString();
     }
 
-    // todo: substituir a implementação da tabela Sale_Refund
     private String createSaleRefundTableSql(){
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE SaleRefund (\n");
-        builder.append("id INTEGER NOT NULL PRIMARY KEY, \n");
-        builder.append("sale INTEGER NOT NULL, \n");
-        builder.append("FOREIGN KEY(sale) REFERENCES Sale(id)\n");
+        builder.append("id_sale_refund NUMBER, \n");
+        builder.append("id_sale        NUMBER, \n");
+        builder.append("CONSTRAINT sale_refund_pk PRIMARY KEY (id_sale_refund),\n");
+        builder.append("CONSTRAINT id_sale_refund_fk FOREIGN KEY (id_sale) REFERENCES sale(id_sale)");
         builder.append("); \n");
 
         System.out.println(builder.toString());
         return builder.toString();
     }
 
-    // todo: a implementação da tabela Customer_Buys
-    private String createCustomerBuysTableSql() {}
+    private String createCustomerBuysTableSql() {
+        StringBuilder builder = new StringBuilder();
 
-    // todo: a implementação da tabela Supplier_Provides
-    private String createSupplierProvidesTableSql() {}
+        builder.append("CREATE TABLE customer_Buys ( \n");
+        builder.append("id_product NUMBER, \n");
+        builder.append("id_customer NUMBER,\n");
+        builder.append("sale_date TIMESTAMP,\n");
+        builder.append("price NUMBER NOT NULL,\n");
+        builder.append("quantity NUMBER NOT NULL,\n");
+        builder.append("CONSTRAINT customer_buys_pk PRIMARY KEY (id_product, id_customer, sale_date),\n");
+        builder.append("CONSTRAINT price_customer_ck CHECK (price > 0),\n");
+        builder.append("CONSTRAINT quantity_buyies_ck CHECK (quantity > 0)\n");
+        builder.append("); \n");
+
+        System.out.println(builder.toString());
+        return builder.toString();
+    }
+
+    private String createSupplierProvidesTableSql() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("CREATE TABLE supplier_provides(\n");
+        builder.append("id_supplier NUMBER,\n");
+        builder.append("id_product NUMBER,\n");
+        builder.append("purchase_date TIMESTAMP,\n");
+        builder.append("price NUMBER NOT NULL,\n");
+        builder.append("quantity NUMBER NOT NULL,\n");
+        builder.append("CONSTRAINT supplier_provides_pk PRIMARY KEY (id_supplier, id_product, purchase_date),\n");
+        builder.append("CONSTRAINT price_supplies_ck CHECK (price > 0),\n");
+        builder.append("CONSTRAINT quantity_supplies CHECK (quantity > 0)\n");
+        builder.append(");");
+
+        System.out.println(builder.toString());
+        return builder.toString();
+    }
 }
