8fe193ecb0b3c52fbccc91d2e7c10237c3b63a60
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-13 18:11:16 -0300

    feat: added AUTOINCREMENT clause to most of the ids

--- src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/application/repositories/sqlite/util/SQLiteDatabaseBuilder.java
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/application/repositories/sqlite/util/SQLiteDatabaseBuilder.java
@@ -46,13 +46,12 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE Customer (\n");
-        builder.append("id_customer NUMBER NOT NULL, \n");
+        builder.append("id_customer INTEGER PRIMARY KEY AUTOINCREMENT, \n");
         builder.append("name VARCHAR(100) NOT NULL, \n");
         builder.append("CPF VARCHAR(15) NOT NULL, \n");
         builder.append("address VARCHAR(100), \n");
         builder.append("phone_Number VARCHAR(20), \n");
         builder.append("status VARCHAR(10) DEFAULT 'ACTIVE', \n");
-        builder.append("CONSTRAINT customer_pk PRIMARY KEY (id_customer),");
         builder.append("CONSTRAINT cpf_un UNIQUE (CPF)");
         builder.append(");\n");
 
@@ -63,12 +62,11 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE Product (\n");
-        builder.append("id_product NUMBER NOT NULL, \n");
+        builder.append("id_product INTEGER PRIMARY KEY AUTOINCREMENT, \n");
         builder.append("name VARCHAR(100) NOT NULL, \n");
         builder.append("quantity NUMBER NOT NULL, \n");
         builder.append("average_Purchase_Price NUMBER, \n");
         builder.append("status VARCHAR(10) NOT NULL DEFAULT 'ACTIVE', \n");
-        builder.append("CONSTRAINT product_pk PRIMARY KEY (id_product), \n");
         builder.append("CONSTRAINT name_un UNIQUE (name), \n");
         builder.append("CONSTRAINT quantity_ck CHECK (quantity >= 0) \n");
         builder.append("CONSTRAINT average_purchase_price_ck CHECK (average_purchase_price > 0) \n");
@@ -81,12 +79,11 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE Supplier (\n");
-        builder.append("id_supplier NUMBER, \n");
+        builder.append("id_supplier INTEGER PRIMARY KEY AUTOINCREMENT, \n");
         builder.append("CNPJ VARCHAR(15) NOT NULL, \n");
         builder.append("corporate_Name VARCHAR(100) NOT NULL, \n");
         builder.append("Phone_Number VARCHAR(20), \n");
         builder.append("status VARCHAR(10) NOT NULL DEFAULT 'ACTIVE',");
-        builder.append("CONSTRAINT supplier_pk PRIMARY KEY (id_supplier),");
         builder.append("CONSTRAINT CNPJ_UN UNIQUE (CNPJ),");
         builder.append("CONSTRAINT corporate_name_un UNIQUE (corporate_name)");
         builder.append("); \n");
@@ -98,9 +95,9 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE Purchase (\n");
-        builder.append("id_purchase NUMBER, \n");
-        builder.append("id_supplier NUMBER NOT NULL, \n");
-        builder.append("id_product NUMBER NOT NULL, \n");
+        builder.append("id_purchase INTEGER, \n");
+        builder.append("id_supplier INTEGER NOT NULL, \n");
+        builder.append("id_product INTEGER NOT NULL, \n");
         builder.append("purchase_date TIMESTAMP NOT NULL,\n");
         builder.append("CONSTRAINT purchase_pk PRIMARY KEY (id_purchase),");
         builder.append("CONSTRAINT purchase_supplier_supplies_fk FOREIGN KEY (id_supplier, id_product, purchase_date)");
@@ -114,9 +111,8 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE Purchase_Refund (\n");
-        builder.append("id_purchase_refund NUMBER, \n");
-        builder.append("id_purchase NUMBER, \n");
-        builder.append("CONSTRAINT purchase_refund_pk PRIMARY KEY (id_purchase_refund),\n");
+        builder.append("id_purchase_refund INTEGER PRIMARY KEY AUTOINCREMENT, \n");
+        builder.append("id_purchase INTEGER, \n");
         builder.append("CONSTRAINT id_purchase_refund_fk FOREIGN KEY (id_purchase) REFERENCES purchase(id_purchase)");
         builder.append("); \n");
 
@@ -127,8 +123,8 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE customer_Buys ( \n");
-        builder.append("id_product NUMBER, \n");
-        builder.append("id_customer NUMBER,\n");
+        builder.append("id_product INTEGER, \n");
+        builder.append("id_customer INTEGER,\n");
         builder.append("sale_date TIMESTAMP,\n");
         builder.append("price NUMBER NOT NULL,\n");
         builder.append("quantity NUMBER NOT NULL,\n");
@@ -144,9 +140,9 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE Sale (\n");
-        builder.append("id_sale NUMBER, \n");
-        builder.append("id_customer NUMBER NOT NULL, \n");
-        builder.append("id_product NUMBER NOT NULL, \n");
+        builder.append("id_sale INTEGER, \n");
+        builder.append("id_customer INTEGER NOT NULL, \n");
+        builder.append("id_product INTEGER NOT NULL, \n");
         builder.append("sale_date TIMESTAMP NOT NULL,");
         builder.append("CONSTRAINT sale_pk PRIMARY KEY (id_sale),\n");
         builder.append("CONSTRAINT sale_customer_buys_fk FOREIGN KEY (id_product, id_customer, sale_date)");
@@ -160,8 +156,8 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE SaleRefund (\n");
-        builder.append("id_sale_refund NUMBER, \n");
-        builder.append("id_sale        NUMBER, \n");
+        builder.append("id_sale_refund INTEGER PRIMARY KEY AUTOINCREMENT, \n");
+        builder.append("id_sale        INTEGER, \n");
         builder.append("CONSTRAINT sale_refund_pk PRIMARY KEY (id_sale_refund),\n");
         builder.append("CONSTRAINT id_sale_refund_fk FOREIGN KEY (id_sale) REFERENCES sale(id_sale)");
         builder.append("); \n");
@@ -173,8 +169,8 @@ public final class SQLiteDatabaseBuilder implements DataBaseBuilder {
         StringBuilder builder = new StringBuilder();
 
         builder.append("CREATE TABLE supplier_provides(\n");
-        builder.append("id_supplier NUMBER,\n");
-        builder.append("id_product NUMBER,\n");
+        builder.append("id_supplier INTEGER,\n");
+        builder.append("id_product INTEGER,\n");
         builder.append("purchase_date TIMESTAMP,\n");
         builder.append("price NUMBER NOT NULL,\n");
         builder.append("quantity NUMBER NOT NULL,\n");
