efc5638e749d100dcd3a2eaa1931bc2125e338da
AlexandreStrpasson <113216865+AlexandreStrpasson@users.noreply.github.com>
2023-06-13 14:58:53 -0300

    Developed the SaleUI and SaleManegementUI

---
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/application/controller/SaleManegementUI.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.aluno.ddos4.construfacil.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class SaleManegementUI {
+    public void insertSale(ActionEvent actionEvent) {
+    }
+
+    public void updateSale(ActionEvent actionEvent) {
+    }
+
+    public void back(ActionEvent actionEvent) {
+    }
+}
