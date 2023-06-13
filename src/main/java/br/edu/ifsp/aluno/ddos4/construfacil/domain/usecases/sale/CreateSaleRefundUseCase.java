64d2a5eb07131a07c999e8fae17bb81e68137cac
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-13 00:17:08 -0300

    feat: implemented CreateSaleRefundUseCase

---
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/domain/usecases/sale/CreateSaleRefundUseCase.java
@@ -0,0 +1,33 @@
+package br.edu.ifsp.aluno.ddos4.construfacil.domain.usecases.sale;
+
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.sale.Sale;
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.entities.sale.SaleRefund;
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.persistence.dao.SaleRefundDAO;
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.usecases.util.EntityAlreadyExistsException;
+import br.edu.ifsp.aluno.ddos4.construfacil.domain.usecases.util.EntityNotFoundException;
+
+public class CreateSaleRefundUseCase {
+    private final SaleRefundDAO saleRefundDAO;
+    private final FindSaleUseCase findSaleUseCase;
+
+    public CreateSaleRefundUseCase(SaleRefundDAO saleRefundDAO, FindSaleUseCase findSaleUseCase) {
+        this.saleRefundDAO = saleRefundDAO;
+        this.findSaleUseCase = findSaleUseCase;
+    }
+
+    public Long createRefund(long saleId) {
+        Sale sale = findSaleUseCase.findOneById(saleId)
+                .orElseThrow(() -> new EntityNotFoundException("There is not such sale!"));
+
+        saleRefundDAO.findOneBySale(sale)
+                .ifPresent(s -> {
+                    throw new EntityAlreadyExistsException("There already is a refund for this Sale!");
+                });
+
+        saleRefundDAO.save(new SaleRefund(sale));
+
+        return saleRefundDAO.findOneBySale(sale)
+                            .orElseThrow(() -> new EntityNotFoundException("Could not save the refund!"))
+                            .getId();
+    }
+}
