5e7b6f1a6102b7b72748cabdead34681087086a9
AlexandreStrpasson <113216865+AlexandreStrpasson@users.noreply.github.com>
2023-06-13 14:43:01 -0300

    Developed the PurchaseManegementUI and PurchaseManegementUI

---
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/application/controller/PurchaseManegementUI.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.aluno.ddos4.construfacil.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class PurchaseManegementUI {
+    public void insertPurchase(ActionEvent actionEvent) {
+    }
+
+    public void updatePurchase(ActionEvent actionEvent) {
+    }
+
+    public void back(ActionEvent actionEvent) {
+    }
+}
