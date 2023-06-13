9443ef99e8e98ccf7bcbc2500039a6e322de8f99
AlexandreStrpasson <113216865+AlexandreStrpasson@users.noreply.github.com>
2023-06-13 14:20:11 -0300

    Developed the ProductManegementUI and ProductUI

---
+++ src/main/java/br/edu/ifsp/aluno/ddos4/construfacil/application/controller/ProductManegementUI.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.aluno.ddos4.construfacil.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class ProductManegementUI {
+    public void insertProduct(ActionEvent actionEvent) {
+    }
+
+    public void editProduct(ActionEvent actionEvent) {
+    }
+
+    public void back(ActionEvent actionEvent) {
+    }
+}
